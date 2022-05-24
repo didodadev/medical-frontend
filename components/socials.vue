425bf5a271bcc9127c60b1a143796d5efcb20c9b
polat-poyraz <poyrazpolat647@gmail.com>
2022-05-24 12:56:09 +0300

    Sosyal media iconlari eklendi.

---
+++ components/socials.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="socials">
+    <div class="item">
+      <i class="bi bi-instagram"></i>
+    </div>
+
+    <div class="item">
+      <i class="bi bi-facebook"></i>
+    </div>
+
+    <div class="item">
+      <i class="bi bi-youtube"></i>
+    </div>
+  </div>
+</template>
+
+<style>
+:root {
+  --social-item-height: 2em;
+}
+
+.socials {
+  position: fixed;
+  right: -1px;
+  top: calc(50% - ((var(--social-item-height) * 3) / 2));
+  border: 1px solid var(--border-2);
+  border-top-left-radius: 7px;
+  border-bottom-left-radius: 7px;
+  background: white;
+}
+
+.socials .item {
+  height: var(--social-item-height);
+  width: var(--social-item-height);
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  font-size: 18px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="default-template container-fluid">
+  <div class="default-template container-fluid p-0">
     <TopSideBarVue />
     <HeaderVue />
+    <SocialsVue />
     <Nuxt />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import TopSideBarVue from '../components/top-side-bar.vue'
-import HeaderVue from '../components/header.vue'
+import Vue from "vue";
+import TopSideBarVue from "../components/top-side-bar.vue";
+import HeaderVue from "../components/header.vue";
+import SocialsVue from "../components/socials.vue";
 
 export default Vue.extend({
   components: {
     TopSideBarVue,
-    HeaderVue
-  }
-})
+    HeaderVue,
+    SocialsVue,
+  },
+});
 </script>
-v
