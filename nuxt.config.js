b0e1a3cf4595e6eb45619aaf3ccf93b5f80b2ba1
polat-poyraz <poyrazpolat647@gmail.com>
2022-05-24 17:34:38 +0300

    Bootstrap eklendi.

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/antd-ui'
+    '@/plugins/antd-ui',
+    '@/plugins/bootstrap'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
